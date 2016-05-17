38468afd21bc64603c7a8694782a20adadccf5f3
miorelv8 <Dumb18@mail.ru>
2016-05-17 09:55:39 +0300

    chtoto dobavil

---
+++ test/main.js
@@ -0,0 +1,10 @@
+// var myName="jenya";
+// console.log(myName);
+// myName="vasia";
+myName="Jenya";
+console.log(myName); 
+var myName;
+console.log(myName);
+myName = "Vasya";
+
+console.log(myName);
